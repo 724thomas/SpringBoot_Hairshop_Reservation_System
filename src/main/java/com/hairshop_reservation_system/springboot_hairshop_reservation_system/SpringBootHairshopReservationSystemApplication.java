a654361950739b67789c71e64741d399179e115d
WonJoonChoi <724thomas@gmail.com>
2023-01-29 10:33:14 +0900

    Initial commit

---
+++ src/main/java/com/hairshop_reservation_system/springboot_hairshop_reservation_system/SpringBootHairshopReservationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.hairshop_reservation_system.springboot_hairshop_reservation_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHairshopReservationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootHairshopReservationSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hairshop_reservation_system/springboot_hairshop_reservation_system/SpringBootHairshopReservationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hairshop_reservation_system.springboot_hairshop_reservation_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootHairshopReservationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
